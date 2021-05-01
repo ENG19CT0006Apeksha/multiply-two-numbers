d8d4404cb8c2489ed1bc1836e28472023ff3fab4
Apekshaprabhu <>
2021-05-01 08:28:29 +0000

    answer

--- multiply.c
+++ multiply.c
@@ -1,1 +1,13 @@
 //WAP in c to multiply two numbers
+#include<stdio.h>
+int main()
+{
+  int a;
+  int b;
+  int product;
+  
+ scanf("%d",&a);
+ scanf("%d",&b);
+ product=a*b;
+ printf("%d",product);
+}
